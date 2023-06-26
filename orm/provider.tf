861b04e5ebc3a0127c33e0292ae1546fb210b83e
Eli Schilling <schilling.eli@gmail.com>
2023-06-26 09:48:13 -0700

    Bump OKE and TF versions

--- orm/provider.tf
+++ orm/provider.tf
@@ -2,7 +2,7 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.1"
   required_providers {
     oci = {
       source = "oracle/oci"
